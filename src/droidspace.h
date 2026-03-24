c4d59dad6f4c0289ab42e4a31f54f8af89f61b37
ravindu644 <droidcasts@protonmail.com>
2026-03-24 20:52:11 +0530

    droidspaces: bump v5.8.1-hotfix
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- src/droidspace.h
+++ src/droidspace.h
@@ -60,7 +60,7 @@
  * ---------------------------------------------------------------------------*/
 
 #define DS_PROJECT_NAME "Droidspaces"
-#define DS_VERSION "5.8.0"
+#define DS_VERSION "5.8.1"
 #define DS_MIN_KERNEL_MAJOR 3
 #define DS_MIN_KERNEL_MINOR 18
 #define DS_RECOMMENDED_KERNEL_MAJOR 4
